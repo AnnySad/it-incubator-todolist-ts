273753f2e8d8cecb96c70cb6575d7e23ca3e10f9
AnnySad <anna.sadovskaya555@gmail.com>
2022-08-23 17:38:44 +0300

    Expand tasksReducer: addTodolist +general ID

--- src/state/tasksReducer.ts
+++ src/state/tasksReducer.ts
@@ -73,7 +73,7 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
         }
         case "ADD-TODOLIST":{
             const stateCopy={...state}
-            stateCopy[v1()] =[]
+            stateCopy[action.todolistId] =[]
             return stateCopy
         }
         default:
